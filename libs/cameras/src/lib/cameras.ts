29c504fe6b9040accc3bf4f8cddb58ff13ba52f1
Julio Sansossio <juliosansossio@gmail.com>
2021-06-14 16:02:30 +0200

    update cameras

--- libs/cameras/src/lib/cameras.ts
+++ libs/cameras/src/lib/cameras.ts
@@ -1,12 +1,12 @@
 export const RTSP_CAMERAS = [
   {
-    name: 'random',
-    input: 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov',
+    name: '360',
+    input: 'rtsp://testing:testing@192.168.1.38/stream2', // stream1 = 1080
     quality: 0
   },
   {
-    name: 'house',
-    input: 'rtsp://testing:testing@192.168.1.38/stream2',
+    name: 'random',
+    input: 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov',
     quality: 0
   }
 ]
