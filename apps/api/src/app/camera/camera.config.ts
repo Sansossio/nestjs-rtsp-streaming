d014e97afeef4d2aac548164f1f4857925011f31
Julio Sansossio <juliosansossio@gmail.com>
2021-06-14 14:51:09 +0200

    ignore random camera

--- apps/api/src/app/camera/camera.config.ts
+++ apps/api/src/app/camera/camera.config.ts
@@ -3,5 +3,10 @@ export const RTSP_CAMERAS = [
     name: 'random',
     input: 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov',
     quality: 0
+  },
+  {
+    name: 'house',
+    input: 'rtsp://testing:testing@192.168.1.38/stream2',
+    quality: 0
   }
 ]
